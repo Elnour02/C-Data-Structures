e0582c064ccd25a662ed05f119362e0a7cc26fb8
Nour Yazbek <nour02beck@gmail.com>
2026-03-08 18:11:13 +0100

    initial commit

---
+++ LinkedList/src/linkedList.c
@@ -0,0 +1,147 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/linkedList.h"
+
+void initList(struct List* list) {
+    if (list != NULL) {
+        list->first = NULL;
+        list->numOfNodes = 0;
+    }
+    else printf("List is NULL\n");
+}
+
+void addFirst(struct List* list, int data) {
+    if (list != NULL) {
+        struct Node* newNode = (struct Node*) malloc(sizeof(struct Node));
+        if (newNode != NULL) {
+            newNode->data = data;
+            newNode->next = list->first;
+            list->first = newNode;
+            list->numOfNodes++;
+        }
+        else printf("Memory allocation failed\n");
+    }
+    else printf("List is NULL\n");
+}
+
+void addLast(struct List* list, int data) {
+    if (list == NULL) printf("List is NULL\n");
+    else if (list->first == NULL) addFirst(list, data);
+    else {
+        struct Node* node = list->first;
+        while (node->next != NULL) {
+            node = node->next;
+        }
+        struct Node* newNode = (struct Node*) malloc(sizeof(struct Node));
+        if (newNode != NULL) {
+            newNode->data = data;
+            node->next = newNode;
+            newNode->next = NULL;
+            list->numOfNodes++;
+        }
+        else printf("Memory allocation failed\n");
+    }
+}
+
+void removeFirst(struct List* list) {
+    if (list == NULL) printf("List is NULL\n");
+    else if (list->first == NULL) printf("List is empty\n");
+    else {
+        struct Node* node = list->first;
+        list->first = node->next;
+        free(node);
+        list->numOfNodes--;
+    }
+}
+
+void removeLast(struct List* list) {
+    if (list == NULL) printf("List is NULL\n");
+    else if (list->first == NULL) printf("List is empty\n");
+    else if (list->numOfNodes == 1) removeFirst(list);
+    else {
+        struct Node* node = list->first;
+        while (node->next->next != NULL) {
+            node = node->next;
+        }
+        struct Node* pNode = node;
+        free(node->next);
+        pNode->next = NULL;
+        list->numOfNodes--;
+    }
+}
+
+int getFirst(struct List *list) {
+    if (list == NULL) printf("List is NULL\n");
+    else if (list->first == NULL) printf("List is empty\n");
+    else return list->first->data;
+    return INT_MIN;
+}
+
+int getLast(struct List* list) {
+    if (list == NULL) printf("List is NULL\n");
+    else if (list->first == NULL) printf("List is empty\n");
+    else {
+        struct Node* node = list->first;
+        while (node->next != NULL) {
+            node = node->next;
+        }
+        return node->data;
+    }
+    return INT_MIN;
+}
+
+int contains(struct List* list, int value) {
+    struct Node* node = list->first;
+    while (node != NULL) {
+        if (node->data == value) return 1;
+        node = node->next;
+    }
+    return 0;
+}
+
+void sortList(struct List* list) {
+    if (list == NULL) printf("List is NULL\n");
+    else if (list->first == NULL) printf("List is empty\n");
+    else {
+        struct Node* node;
+        int temp;
+        for (int i = 0; i < list->numOfNodes-1; i++) {
+            node = list->first;
+            while (node->next != NULL) {
+                if (node->data > node->next->data) {
+                    temp = node->data;
+                    node->data = node->next->data;
+                    node->next->data = temp;
+                }
+                node = node->next;
+            }
+        }
+    }
+}
+
+void printList(struct List* list) {
+    if (list == NULL) printf("List is NULL\n");
+    else if (list->first == NULL) printf("List is empty\n");
+    else {
+        struct Node *node = list->first;
+        while (node->next != NULL) {
+            printf("%d -> ", node->data);
+            node = node->next;
+        }
+        printf("%d\n", node->data);
+    }    
+}
+
+void freeList(struct List* list) {
+    if (list != NULL) {
+        struct Node* pNode = list->first;
+        struct Node* node;
+        while (pNode != NULL) {
+            node = pNode;
+            pNode = node->next;
+            free(node);
+        }
+        list->first = NULL;
+    }
+    else printf("List is NULL\n");
+}
---
+++ LinkedList/src/main.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/linkedList.h"
+
+int main() {
+    struct List list;
+    initList(&list);
+    int choice;
+    int value;
+    int first;
+    int last;
+    int contain;
+    do {
+        printf("quit 0\naddFirst 1\naddLast 2\nremoveFirst 3\nremoveLast 4\ngetFirst 5\ngetLast 6\ncontains 7\nsortList 8\n\nchoice: ");
+        scanf("%d", &choice);
+        switch (choice) {
+            case 1:
+                printf("\n");
+                printf("Value: ");
+                scanf("%d", &value);
+                addFirst(&list, value);
+                printf("\n");
+                printList(&list);
+                break;
+            case 2:
+                printf("\n");
+                printf("Value: ");
+                scanf("%d", &value);
+                addLast(&list, value);
+                printList(&list);
+                break;
+            case 3:
+                printf("\n");
+                removeFirst(&list);
+                printList(&list);
+                break;
+            case 4:
+                printf("\n");
+                removeLast(&list);
+                printList(&list);
+                break;
+            case 5:
+                printf("\n");
+                first = getFirst(&list);
+                if (first != INT_MIN) printf("First value is %d\n", first);
+                else printf("List is empty\n");
+                break;
+            case 6:
+                printf("\n");
+                last = getLast(&list);
+                if (last != INT_MIN) printf("Last value is %d\n", last);
+                else printf("List is empty\n");
+                break;
+            case 7:
+                printf("\n");
+                printf("Value: ");
+                scanf("%d", &value);
+                contain = contains(&list, value);
+                printf("\n");
+                if (contain) printf("The list contains %d\n", value);
+                else printf("The list does not contain %d\n", value);
+                break;
+            case 8:
+                printf("\n");
+                sortList(&list);
+                printList(&list);
+                break;
+            default:
+                choice = 0;
+        }
+        printf("\n");
+    } while (choice != 0);
+    freeList(&list);
+    return 0;
+}
